080c0ea936a8ebf3de29d0d6ea22af9bcef003a9
mcfarlmt <mcfarlmt@lafayette.edu>
2021-12-22 11:53:02 -0500

    Initial Commit

---
+++ objects.py
@@ -0,0 +1,17 @@
+class Player(name,population,army,currency,politics,allies,enemies,territories):
+    __myName = ""
+    __myPop = __myArmy = __myCurrency = __myPol = 0
+    __myAlly = __myEnemy = __myTerr = list()
+    def _init_(self,name,population,army,currency,politics,allies,enemies,territories):
+        self.__myName = name
+        self.__myPop = population
+        self.__myArmy = army
+        self.__myCurrency = currency
+        self.__myPol = politics
+        self.__myAlly = allies
+        self.__myEnemy = enemies
+        self.__myTerr = territories
+
+class Card(name,description,):
+    __myName = ""
+t
